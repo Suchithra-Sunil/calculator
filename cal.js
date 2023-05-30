68592a474e5ba33d3113519b4d139f3e80dc4f9f
Suchithra Sunil <suchithramsunil@gmail.com>
2023-05-30 20:11:05 +0530

    initial commit

---
+++ cal.js
@@ -0,0 +1,15 @@
+function displayNum(num) {
+    result.value+=num
+}
+function evaluvateExpr() {
+    // expr=result.value
+    // op=eval(expr)
+    // result.value=op
+    result.value=eval(result.value)
+}
+function allClear() {
+    result.value=" "
+}
+function backSpace() {
+    result.value=result.value.slice(0,-1)
+}
